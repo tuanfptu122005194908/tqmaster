e8b0e3e10b0e748f088638bbf81d3f9f6a02df21
thanhtuanfptse05 <caotuan01122005@gmail.com>
2026-08-06 16:49:56 +0700

    fix: report modal height overflow

--- src/pages/user/ExamPage.tsx
+++ src/pages/user/ExamPage.tsx
@@ -359,6 +359,8 @@ export default function ExamPage() {
               borderRadius: 24, 
               width: '100%', 
               maxWidth: 500, 
+              maxHeight: '90vh',
+              overflowY: 'auto',
               padding: 28, 
               boxShadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)',
               border: '1px solid #e2e8f0',
